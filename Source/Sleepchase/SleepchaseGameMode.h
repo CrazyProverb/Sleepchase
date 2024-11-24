5722c74bdd987181ae7f739182c313d84919b834
YANZHANYUAN <rickyan.s@foxmail.com>
2024-11-25 01:29:33 +0800

    项目初始化

---
+++ Source/Sleepchase/SleepchaseGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SleepchaseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASleepchaseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASleepchaseGameMode();
+};
+
+
+
